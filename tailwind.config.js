1b8494d1b8c2746610aee7ffe6accb4ffec37f9c
sahhoutiamine <midoriaduko@gmail.com>
2025-11-06 19:44:14 +0100

    add cards section and switch the tw link

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/ @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [".//*.{html,js}"], // simplified and cleaner
+  theme: {
+    extend: {
+
+    },
+  },
+  plugins: [],
+};
